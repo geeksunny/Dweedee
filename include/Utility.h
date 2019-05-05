410f89d902c6f7a70646360d3825cd4c8d394d7e
Justin Swanson <geeksunny@gmail.com>
2019-05-04 21:21:22 -0500

    Namespacing the Utility code

--- include/Utility.h
+++ include/Utility.h
@@ -2,13 +2,17 @@
 #define DWEEDEE_UTILITY_H
 
 
-void halt();
+namespace dweedee {
 
-void haltBlinking(unsigned long interval);
-void haltBlinking(unsigned long interval, unsigned int ledPin);
+    void halt();
 
-void haltBlinkingTimed(unsigned long timeOn, unsigned long timeOff);
-void haltBlinkingTimed(unsigned long timeOn, unsigned long timeOff, unsigned int ledPin);
+    void haltBlinking(unsigned long interval);
+    void haltBlinking(unsigned long interval, unsigned int ledPin);
+
+    void haltBlinkingTimed(unsigned long timeOn, unsigned long timeOff);
+    void haltBlinkingTimed(unsigned long timeOn, unsigned long timeOff, unsigned int ledPin);
+
+}
 
 
 #endif //DWEEDEE_UTILITY_H

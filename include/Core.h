43cd00a35179025959c4b8bfa5c6a5146b2231e1
Justin Swanson <geeksunny@gmail.com>
2019-05-05 22:26:51 -0500

    Breaking Hotplug classes out into their own files.

--- include/Core.h
+++ include/Core.h
@@ -1,15 +1,9 @@
 #ifndef DWEEDEE_CORE_H
 #define DWEEDEE_CORE_H
 
-#include <Arduino.h>
-
-#include <SPI.h>
 #include <Usb.h>
-#include <usbhub.h>
 
-#include <ArduinoSTL.h>
-#include <deque>
-
+#include "Hotplug.h"
 #include "UsbDeviceInfo.h"
 
 
@@ -17,39 +11,6 @@ typedef uint8_t UsbDevAddr;
 
 
 namespace dweedee {
-
-    class HotplugEventHandler {
-
-    public:
-        virtual void onDevicesAdded(UsbDeviceInfo **added, short count) = 0;
-        virtual void onDevicesRemoved(UsbDeviceInfo **removed, short count) = 0;
-
-    };
-
-
-    class HotplugManager {
-
-        static HotplugManager *instance;
-        static void checkUsbDevice(UsbDevice *pdev);
-        static std::deque<UsbDeviceInfo*>::deque_iter findDevInfo(std::deque<UsbDeviceInfo*> *deque, uint8_t usbDevAddr);
-
-        USB *Usb_;
-        USBHub *Hub_;
-        unsigned short int devicesAdded_ = 0;
-        std::deque<UsbDeviceInfo*> usbDeviceIndex_;
-        std::deque<UsbDeviceInfo*> usbDeviceQueue_;
-        HotplugEventHandler *eventHandler_;
-
-        UsbDeviceInfo* getDeviceInfo(UsbDevice *pdev);
-        char* getStringDescriptor(byte usbDevAddr, byte strIndex);
-        void resetUsbDevAddrQueue();
-
-    public:
-        explicit HotplugManager(USB *Usb, HotplugEventHandler *eventHandler);
-        void task();
-        void processUsbDevice(UsbDevice *pdev);
-
-    };
 
 
     class Core : HotplugEventHandler {
---
+++ include/Hotplug.h
@@ -0,0 +1,55 @@
+#ifndef DWEEDEE_HOTPLUG_H
+#define DWEEDEE_HOTPLUG_H
+
+#include <usbhub.h>
+
+#include <ArduinoSTL.h>
+#include <deque>
+
+#include "UsbDeviceInfo.h"
+
+
+#define USB_TYPE_HUB    0x29
+
+
+namespace dweedee {
+
+
+    class HotplugEventHandler {
+
+    public:
+        virtual void onDevicesAdded(UsbDeviceInfo **added, short count) = 0;
+        virtual void onDevicesRemoved(UsbDeviceInfo **removed, short count) = 0;
+
+    };
+
+
+    class HotplugManager {
+
+        static HotplugManager *instance;
+        static void checkUsbDevice(UsbDevice *pdev);
+        static std::deque<UsbDeviceInfo*>::deque_iter findDevInfo(std::deque<UsbDeviceInfo*> *deque, uint8_t usbDevAddr);
+
+        USB *Usb_;
+        unsigned short int devicesAdded_ = 0;
+        std::deque<USBHub*> usbHubs_;
+        std::deque<UsbDeviceInfo*> usbDeviceIndex_;
+        std::deque<UsbDeviceInfo*> usbDeviceQueue_;
+        HotplugEventHandler *eventHandler_;
+
+        UsbDeviceInfo* getDeviceInfo(UsbDevice *pdev);
+        char* getStringDescriptor(byte usbDevAddr, byte strIndex);
+        void resetUsbDevAddrQueue();
+
+    public:
+        explicit HotplugManager(USB *Usb, HotplugEventHandler *eventHandler);
+        void task();
+        void processUsbDevice(UsbDevice *pdev);
+
+    };
+
+
+}
+
+
+#endif //DWEEDEE_HOTPLUG_H
